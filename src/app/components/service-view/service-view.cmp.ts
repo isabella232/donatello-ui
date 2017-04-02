9bdec82c4f89ca1322242cdd689fdeb4e3caa897
Vlad Ioffe <vlio20@gmail.com>
2017-04-02 13:21:50 +0300

    enabling router

--- src/app/components/service-view/service-view.cmp.ts
+++ src/app/components/service-view/service-view.cmp.ts
@@ -1,14 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MockService} from '../../services/mock-service/mock.srv';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'do-service-view',
   template: require('./service-view.html'),
   styles: [require('./service-view.less').toString()]
 })
-export class ServiceView {
-  name = 'Angular';
+export class ServiceView implements OnInit {
+  id: string;
 
-  constructor(private mockService: MockService) {
+  constructor(private mockService: MockService, private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.id = params['id'];
+    })
   }
 }
